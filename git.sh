c3c550c8b76eb00f689b33d5865b01bf41dc3c69
asmtask <asmtask@gmai.com>
2016-05-09 06:10:54 +0000

    first commit

---
+++ git.sh
@@ -0,0 +1,9 @@
+
+echo "# nginx-1.9.15" >> README.md
+git init
+git add *
+git commit -m "first commit"
+git remote add origin https://github.com/asmlib/nginx-1.9.15.git
+git push -u origin master
+
+
